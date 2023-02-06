1f7c16fc26bdb0735f86968b0efe2be21e19719b
George <georgie817@gmail.com>
2023-02-06 16:50:50 +0300

    fix on mobile ui

--- resources/js/Pages/Usajiri/Usajiri.vue
+++ resources/js/Pages/Usajiri/Usajiri.vue
@@ -49,7 +49,6 @@ const storeUsajiri = () => {
             flex
             items-top
             justify-center
-            min-h-screen
             bg-gray-100
             dark:bg-gray-900
             sm:items-center sm:pt-0
